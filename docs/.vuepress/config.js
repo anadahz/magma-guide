2ab7fd322a1aa25187a590536569f185c05b9e1b
anadahz <anadahz@users.noreply.github.com>
2025-11-26 00:13:14 +0000

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,31 +1,34 @@
-module.exports = {
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
   title: 'magma',
   description: 'This is the magma guide',
-  base: process.env.VUEPRESS_BASE || '/',
-  dest: 'public',
+  base: '/magma-guide/',
+  outDir: 'public',
   head: [
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' }],
-    ['link', { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/icons/favicon-16x16.png' }],
-    ['link', { rel: 'icon', type: 'image/png', sizes: "32x32", href: '/icons/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['link', { rel: 'mask-icon', color: '#5bbad5', href: '/icons/safari-pinned-tab.svg' }],
-    ['meta', { name: "msapplication-config", content: "/browserconfig.xml"}],
+    ['meta', { name: 'msapplication-config', content: '/browserconfig.xml' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['meta', { name: 'msapplication-TileColor', content: '#603cba' }],
     ['meta', { name: 'msapplication-TileImage', content: '/icons/mstile-144x144.png' }],
-    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
   ],
+
   themeConfig: {
-    repo: 'https://github.com/lavafeld/magma-guide',
-    editLinks: true,
+    repo: 'lavafeld/magma-guide',
+    editLink: {
+      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      text: 'Help us improve this page!'
+    },
     docsDir: 'docs',
     smoothScroll: true,
-    editLinkText: 'Help us improve this page!',
     lastUpdated: 'Last Updated',
     sidebar: {
-      // https://github.com/vuejs/vuepress/issues/984
-      sidebarDepth: 2,
       '/guide/': getGuideSidebar('Guide', 'Advanced', 'Contribute'),
     },
     nav: [
@@ -37,53 +40,39 @@ module.exports = {
       { text: 'Contact', link: 'https://blog.magma.lavafeld.org/contact/' },
       { text: 'Blog', link: 'https://blog.magma.lavafeld.org/' }
     ]
-  },
-  plugins: {
-      '@vuepress/back-to-top': true,
-      'vuepress-plugin-export': true,
-      'vuepress-plugin-zooming': true,
-      'vuepress-plugin-one-click-copy': {
-        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'],
-        copyMessage: 'Copied!',
-        duration: 600,
-        showInMobile: false
-      },
-      'check-md': true
   }
-}
+})
 
-function getGuideSidebar (groupA, groupB, groupC) {
+function getGuideSidebar(groupA, groupB, groupC) {
   return [
     {
-      title: groupA,
-      collapsable: false,
-      children: [
-        '',
-        'getting-started',
-        'testing-methodology',
-        'network-measurements',
-        'run-measurements',
-        'data-analysis',
-	'censorship-circumvention',
-        'glossary'
+      text: groupA,
+      collapsed: false,
+      items: [
+        { text: '', link: '/guide/' },
+        { text: 'Getting Started', link: '/guide/getting-started' },
+        { text: 'Testing Methodology', link: '/guide/testing-methodology' },
+        { text: 'Network Measurements', link: '/guide/network-measurements' },
+        { text: 'Run Measurements', link: '/guide/run-measurements' },
+        { text: 'Data Analysis', link: '/guide/data-analysis' },
+        { text: 'Censorship Circumvention', link: '/guide/censorship-circumvention' },
+        { text: 'Glossary', link: '/guide/glossary' }
       ]
     },
     {
-      title: groupB,
-      collapsable: false,
-      children: [
-        'advanced-intro',
-      //'analyze-dns',
-      //'analyze-google-traffic-data',
-        'data-sources',
-        'osint-sources'
+      text: groupB,
+      collapsed: false,
+      items: [
+        { text: 'Advanced Intro', link: '/guide/advanced-intro' },
+        { text: 'Data Sources', link: '/guide/data-sources' },
+        { text: 'OSINT Sources', link: '/guide/osint-sources' }
       ]
     },
     {
-      title: groupC,
-      collapsable: false,
-      children: [
-        'contribute'
+      text: groupC,
+      collapsed: false,
+      items: [
+        { text: 'Contribute', link: '/guide/contribute' }
       ]
     }
   ]

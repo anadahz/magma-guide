f3184b9f0a6a313e3852f10da1889910acdd281a
anadahz <anadahz@users.noreply.github.com>
2025-11-26 00:28:06 +0000

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,6 @@ export default defineConfig({
   title: 'magma',
   description: 'This is the magma guide',
   base: '/magma-guide/',
-  outDir: 'public',
   head: [
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' }],

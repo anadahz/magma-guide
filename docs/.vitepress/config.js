a57c9f00d5da9c8de41d5b84455c403af288c271
Nikolas <nikolakisae@gmail.com>
2026-04-28 20:57:04 +0300

    revert all changes

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vitepress'
-import { fileURLToPath } from 'url'
-import path from 'path'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   title: 'magma',
   description: 'This is the magma guide',
   base: '/magma-guide/',
-  outDir: path.resolve(__dirname, '../dist'),
-  publicDir: path.resolve(__dirname, 'public'), 
+  outDir: 'public',
   head: [
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' }],

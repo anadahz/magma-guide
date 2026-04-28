dbbe1a8a24e1590ac4a1bd404055ba555f2ebc69
Nikolas <nikolakisae@gmail.com>
2026-04-28 18:21:17 +0300

    Update outDir dir

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   description: 'This is the magma guide',
   base: '/magma-guide/',
   outDir: 'dist',                     
-  publicDir: '.vitepress/public',
+  publicDir: 'docs/.vitepress/public',
   head: [
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' }],
